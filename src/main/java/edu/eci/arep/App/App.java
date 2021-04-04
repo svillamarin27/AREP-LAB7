ea32a4bf41cb4b34ba4a0c7643b04ae3c7b9e516
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-04-04 12:09:40 -0500

    local

---
+++ src/main/java/edu/eci/arep/App/App.java
@@ -0,0 +1,63 @@
+package edu.eci.arep.App;
+
+import static spark.Spark.*;
+import spark.Request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import spark.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class App {
+	public static void main(String[] args) {
+		port(getPort());
+		get("/datos", (req, res) -> inputDataPage(req, res));
+		get("/results", (req, res) -> resultsPage(req, res));  
+	}
+	static int getPort() {
+		 if (System.getenv("PORT") != null) {
+			 return Integer.parseInt(System.getenv("PORT"));
+		 }
+		 return 4567; 
+		 }
+
+	private static String inputDataPage(Request req, Response res) {
+        String page
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body style=\"background-color:#B9C5BC;\">"
+                +"<font align=\"center\" color=\"Green\" face=\"Comic Sans MS,arial\">"
+                + "<h1>Convertidor de grados farenheit en grados celcius</h1>"
+                + "<form action=\"/results\">"
+                + "  Ingrese unicamente valores en grados farenheit puesto que su respuesta sera en grados celcius <br>"
+                + "  <input type=\"text\" name=\"numbers\" >"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "</body>"
+                + "</html>";
+        return page;
+    }
+	
+	private static String resultsPage(Request req, Response res) {
+        int ent= Integer.parseInt(String.valueOf(req));
+        String result="";
+        try {
+            URL url = new URL(   "" + "?value=" + ent);
+            String cad;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            while ((cad = reader.readLine()) != null) {
+                result = result + cad;
+            }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+		
+}
---
+++ src/main/java/edu/eci/arep/App/Convertidor.java
@@ -0,0 +1,5 @@
+package edu.eci.arep.App;
+
+public class Convertidor {
+	public static Integer square(Integer f){return (f - 32 ) * 5 / 9;}
+}
